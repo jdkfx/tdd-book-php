b16aff262b6a5d7e8ae4d424c4462560d8a5a781
jdkfx <amateurranch@gmail.com>
2022-08-13 14:27:43 +0900

    :rotating_light: Chapter3の実装

---
+++ src/Chapter3/MoneyTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Chapter3;
+
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testMultiplication(): void
+    {
+        $five = new Dollar(5);
+
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
+    }
+
+    /**
+     * @return void
+     */
+    public function testEquality(): void
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+    }
+}
