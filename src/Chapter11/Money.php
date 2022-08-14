1ac63ce0d8b234b289c893b5ddd4220c4dd56eae
jdkfx <amateurranch@gmail.com>
2022-08-14 12:42:00 +0900

    :rotating_light: Chapter11の実装

---
+++ src/Chapter11/Money.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Chapter11;
+
+class Money
+{
+    /**
+     * @var int
+     */
+    protected int $amount;
+
+    /**
+     * @var string
+     */
+    protected string $currency;
+
+    /**
+     * @param int $amount
+     * @param string $currency
+     */
+    public function __construct(int $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Money
+     */
+    public function times(int $multiplier): Money
+    {
+        return new Money($this->amount * $multiplier, $this->currency);
+    }
+
+    /**
+     * @param Money $money
+     * @return bool
+     */
+    public function equals(Money $money): bool
+    {
+        return $this->amount === $money->amount
+            && $this->currency === $money->currency;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->amount . " " . $this->currency;
+    }
+
+    /**
+     * @param int $amount
+     * @return Money
+     */
+    public static function dollar(int $amount): Money
+    {
+        return new Money($amount, "USD");
+    }
+
+    /**
+     * @param int $amount
+     * @return Money
+     */
+    public static function franc(int $amount): Money
+    {
+        return new Money($amount, "CHF");
+    }
+
+    /**
+     * @return string
+     */
+    public function currency(): string
+    {
+        return $this->currency;
+    }
+}
